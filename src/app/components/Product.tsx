c831ab8d85e3113a085ac8fe2713ee8850ae50d6
hikaru ishiwata <hikaru160830@gmail.com>
2024-03-05 21:30:40 +0900

    写真の大きさを変更

--- src/app/components/Product.tsx
+++ src/app/components/Product.tsx
@@ -5,17 +5,17 @@ const Product = () => {
   return (
     <div id="product" className="py-4 w-full">
       <h1 className="text-3xl font-bold mb-4">制作物</h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
         {[1, 2, 3, 4, 5, 6].map((index) => (
           <div
             key={index}
             className="flex flex-col items-center border rounded p-4 transform hover:scale-105 transition duration-300 "
           >
             <Image
-              className="p-2 rounded-lg"
+              className="p-2 w-full h-full object-cover"
               style={{ borderRadius: "1rem" }}
-              width={300}
-              height={300}
+              width={200}
+              height={200}
               alt=""
               src={`https://source.unsplash.com/collection/1346951/1000x1000?sig=${index}`}
             />
