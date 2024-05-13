98bbc5eb1c996f21fbfc5e6d2c9866d127e23c54
Hikaru Ishiwata <hikaru160830@gmail.com>
2024-05-13 21:50:08 +0900

    change 42TOKYO to 42Tokyo

--- src/app/components/Career.tsx
+++ src/app/components/Career.tsx
@@ -8,7 +8,7 @@ const Career = () => {
         <div className="pl-4 py-2">
           <ul className="list-disc">
             <li className="mb-2">2023年 早稲田大学基幹理工学部入学</li>
-            <li className="mb-2">2023年 42TOKYO入学</li>
+            <li className="mb-2">2023年 42Tokyo入学</li>
             <li className="mb-2">2024年 基幹理工学部情報理工学科進学</li>
           </ul>
         </div>
